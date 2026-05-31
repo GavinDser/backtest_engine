2b065c29661cf63590ea245e7bf3dbe1308c4330
GavinDser <jcgeternal@gmail.com>
2026-05-31 15:47:22 -0400

    added config file to reduce repetitive code and ensures boundries

---
+++ src/config.rs
@@ -0,0 +1,5 @@
+#[derive(Debug, Clone, Copy)]
+pub struct BacktestConfig {
+    pub initial_cash: f64,
+    pub commision_rate: f64,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,4 @@ pub mod metrics;
 pub mod portfolio;
 pub mod signal;
 pub mod strategy;
+pub mod config;
